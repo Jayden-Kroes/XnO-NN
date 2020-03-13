eedc9d380dac598f029cd018810ed35c3bab5ef2
JaydenK - Laptop <xtra1nfiNIGHT@gmail.com>
2020-03-13 21:07:42 +1000

    setup NN

--- Main.py
+++ Main.py
@@ -5,7 +5,9 @@ from tic_tac_toe.RandomPlayer import RandomPlayer
 from tic_tac_toe.MinMaxAgent import MinMaxAgent
 from tic_tac_toe.RndMinMaxAgent import RndMinMaxAgent
 from tic_tac_toe.TabularQPlayer import TQPlayer
-
+from tic_tac_toe.SimpleNNQPlayer import NNQPlayer
+from tic_tac_toe.TFSessionManager import TFSessionManager
+import tensorflow as tf
 import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
@@ -89,33 +91,42 @@ def battle(player1: Player = RandomPlayer(), player2: Player = RandomPlayer(), n
     return cross_count, naught_count, draw_count
 
 
-def eval_players(p1 : Player, p2 : Player, num_battles : int, games_per_battle = 100, loc='best'):
+def eval_players(p1 : Player, p2 : Player, num_battles : int = 100, games_per_battle : int = 100, loc='best'):
     p1_wins = []
     p2_wins = []
     draws = []
-    count = []    
+    game_number = []
+    counter = 0
+    
+    TFSessionManager.set_session(tf.Session())
+    TFSessionManager.get_session().run(tf.global_variables_initializer())
 
     for i in range(num_battles):
         p1win, p2win, draw = battle(p1, p2, num_games=games_per_battle, silent=True)
         p1_wins.append(p1win*100.0/games_per_battle)
         p2_wins.append(p2win*100.0/games_per_battle)
         draws.append(draw*100.0/games_per_battle)
-        count.append(i*games_per_battle)
-        p1_wins.append(p1win*100.0/games_per_battle)
-        p2_wins.append(p2win*100.0/games_per_battle)
-        draws.append(draw*100.0/games_per_battle)
-        count.append((i+1)*games_per_battle)
+        counter +=1
+        game_number.append(counter)
 
     plt.ylabel('Game outcomes in %')
     plt.xlabel('Game number')
 
-    plt.plot(count, draws, 'r-', label='Draw')
-    plt.plot(count, p1_wins, 'g-', label='Player 1 wins')
-    plt.plot(count, p2_wins, 'b-', label='Player 2 wins')
+    plt.plot(game_number, draws, 'r-', label='Draw')
+    plt.plot(game_number, p1_wins, 'g-', label='Player 1 wins')
+    plt.plot(game_number, p2_wins, 'b-', label='Player 2 wins')
     plt.legend(loc=loc, shadow=True, fancybox=True, framealpha =0.7)
     plt.show()
 
-player1 = TQPlayer()
-player2 = RandomPlayer()
+    TFSessionManager.set_session(None)
+    return game_number, p1_wins, p2_wins, draws
 
-eval_players(player1, player2, 500)
+
+tf.reset_default_graph()    
+
+nnplayer = NNQPlayer("QLearner1")
+rndplayer = RandomPlayer()
+
+game_number, p1_wins, p2_wins, draws = eval_players(nnplayer, rndplayer)
+
+p = plt.plot(game_number, draws, 'r-', game_number, p1_wins, 'g-', game_number, p2_wins, 'b-')
