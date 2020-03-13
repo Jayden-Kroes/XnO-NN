95151417454b3177fd0f09e32e2e3f36c875c6cd
JaydenK - Laptop <xtra1nfiNIGHT@gmail.com>
2020-03-13 19:37:02 +1000

    add eval players to graph progress

--- Main.py
+++ Main.py
@@ -1,11 +1,14 @@
-# import sys
-# sys.path.append('')
 from IPython.display import HTML, display
-from tic_tac_toe.Board import Board, GameResult, CROSS, NAUGHT
+from tic_tac_toe.Board import Board, GameResult, CROSS, NAUGHT, EMPTY
 from tic_tac_toe.Player import Player
 from tic_tac_toe.RandomPlayer import RandomPlayer
 from tic_tac_toe.MinMaxAgent import MinMaxAgent
 from tic_tac_toe.RndMinMaxAgent import RndMinMaxAgent
+from tic_tac_toe.TabularQPlayer import TQPlayer
+
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
 
 def print_board(board):
     '''
@@ -63,7 +66,7 @@ def play_game(board: Board, player1: Player, player2: Player):
     player2.final_result(final_result)
     return final_result
 
-def battle(player1: Player = RandomPlayer(), player2: Player = RandomPlayer(), num_games: int = 100000):
+def battle(player1: Player = RandomPlayer(), player2: Player = RandomPlayer(), num_games: int = 100000, silent: bool = False):
     board = Board()
     draw_count = 0
     cross_count = 0
@@ -77,15 +80,42 @@ def battle(player1: Player = RandomPlayer(), player2: Player = RandomPlayer(), n
         else:
             draw_count += 1
 
-    print("After {} game we have draws: {}, Player 1 wins: {}, and Player 2 wins: {}.".format(
-        num_games, draw_count, cross_count, naught_count))
-    print("Which gives percentages of draws: {:.2%}, Player 1 wins: {:.2%}, and Player 2 wins:  {:.2%}".format(
-        draw_count / num_games, cross_count / num_games, naught_count / num_games))
+    if not silent:
+        print("After {} game we have draws: {}, Player 1 wins: {}, and Player 2 wins: {}.".format(
+            num_games, draw_count, cross_count, naught_count))
+        print("Which gives percentages of draws: {:.2%}, Player 1 wins: {:.2%}, and Player 2 wins:  {:.2%}".format(
+            draw_count / num_games, cross_count / num_games, naught_count / num_games))
+
+    return cross_count, naught_count, draw_count
 
 
-player1 = RndMinMaxAgent()
-player2 = RndMinMaxAgent()
-board = Board()
+def eval_players(p1 : Player, p2 : Player, num_battles : int, games_per_battle = 100, loc='best'):
+    p1_wins = []
+    p2_wins = []
+    draws = []
+    count = []    
 
-game_count = 100000
-battle(player1, player2)
+    for i in range(num_battles):
+        p1win, p2win, draw = battle(p1, p2, num_games=games_per_battle, silent=True)
+        p1_wins.append(p1win*100.0/games_per_battle)
+        p2_wins.append(p2win*100.0/games_per_battle)
+        draws.append(draw*100.0/games_per_battle)
+        count.append(i*games_per_battle)
+        p1_wins.append(p1win*100.0/games_per_battle)
+        p2_wins.append(p2win*100.0/games_per_battle)
+        draws.append(draw*100.0/games_per_battle)
+        count.append((i+1)*games_per_battle)
+
+    plt.ylabel('Game outcomes in %')
+    plt.xlabel('Game number')
+
+    plt.plot(count, draws, 'r-', label='Draw')
+    plt.plot(count, p1_wins, 'g-', label='Player 1 wins')
+    plt.plot(count, p2_wins, 'b-', label='Player 2 wins')
+    plt.legend(loc=loc, shadow=True, fancybox=True, framealpha =0.7)
+    plt.show()
+
+player1 = TQPlayer()
+player2 = RandomPlayer()
+
+eval_players(player1, player2, 500)
