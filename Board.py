9ec39757dba52b75fb270409bf72d2680442d7fc
JaydenK - Laptop <xtra1nfiNIGHT@gmail.com>
2020-03-13 15:58:26 +1000

    print board

--- Board.py
+++ Board.py
@@ -311,16 +311,3 @@ class Board:
                 print("-----")
 
         print("")
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-50 results found.
--- Main.py
+++ Main.py
@@ -1,4 +1,22 @@
-print("Hello World")
+# import sys
+# sys.path.append('')
+from IPython.display import HTML, display
+from Board import Board, GameResult, CROSS, NAUGHT
 
+'''
+Doesn't work for VSCode
+'''
+def print_board(board):
+    display(HTML("""
+    <style>
+    .rendered_html table, .rendered_html th, .rendered_html tr, .rendered_html td {
+      border: 1px  black solid !important;
+      color: black !important;
+    }
+    </style>
+    """+board.html_str()))
 
+board = Board()
+# print_board(board)
+board.print_board()
 
